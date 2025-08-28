e701c182c513a57b17d7a9a816a586ddfbda2f49
Jovane Maciel de Sousa <jovane.sousa@infrawise.local>
2025-08-28 10:04:30 -0400

    feat: finalizando vendaService

---
+++ VendaService/src/main/java/br/com/jsousa/vendas/online/errorhandling/ApiSubError.java
@@ -0,0 +1,5 @@
+package br.com.jsousa.vendas.online.errorhandling;
+
+abstract class ApiSubError {
+
+}
---
+++ VendaService/src/main/java/br/com/jsousa/vendas/online/exception/BadRequestException.java
@@ -0,0 +1,13 @@
+package br.com.jsousa.vendas.online.exception;
+
+public class BadRequestException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5050912009441441828L;
+
+	public BadRequestException(String msg) {
+		super(msg);
+	}
+}
